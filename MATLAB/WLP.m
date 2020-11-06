84317c1d7bdba01678e56168499a2b5c1854ad5d
ABohynDOE <alexandre.bohyn@kuleuven.be>
2020-11-06 16:05:35 +0100

    Added code explanation

--- MATLAB/WLP.m
+++ MATLAB/WLP.m
@@ -11,15 +11,15 @@ function w = WLP(cols,s)
         cols (1,:) {mustBeVector, mustBeNumeric, mustBePositive}
         s double {mustBeInteger, mustBePositive, mustBeNumeric} = 3
     end
-    S = de2bi(cols)';
-    L = S(:,sum(S)>1);
-    Ls = (L*(-2))+1;
-    model = ff2n(size(L,2));
-    modmat = model(sum(model,2)>0,:);
-    X = x2fx(Ls,modmat);
+    S = de2bi(cols)'; % Reduced design matrix
+    L = S(:,sum(S)>1); % Keep the added factors
+    Ls = (L*(-2))+1; 
+    model = ff2n(size(L,2)); % Enumerate all added factor combination
+    modmat = model(sum(model,2)>0,:); % Only keep 2FI and more
+    X = x2fx(Ls,modmat); % Compute wordlength
     Xf = [abs((X-1)/(-2));sum(modmat,2)'];
     vec = sum(Xf,1);
     edges = 1:max(vec)+1;
-    w = histcounts(vec,edges);
+    w = histcounts(vec,edges); % Summarize into WLP
     w = w(s:end);
 end
--- MATLAB/rLsmaller.m
+++ MATLAB/rLsmaller.m
@@ -1,11 +1,15 @@
 function out = rLsmaller(L1,L2)
+    % rLsmaller Test if a matrix is rL-smaller than another.
+    %   rLsmaller(A,B) returns True if A is rL-smaller than B, and False
+    %   otherwise.
+    
     a = bi2de(L1');
     b = bi2de(L2');
-    mL1 = a(a~=b);
+    mL1 = a(a~=b); % Find first non-identical column in A
     if isempty(mL1)
-        out = false;
+        out = false; % If A and B are similar, returns False
         return
     end
-    mL2 = b(a~=b);
-    out = mL1(1) < mL2(1);
+    mL2 = b(a~=b); % Find first non-identical column in B
+    out = mL1(1) < mL2(1); % Check which if is column in A is rL-smaller than column in B
 end
