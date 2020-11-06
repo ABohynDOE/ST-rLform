19c8cae96e48cebcdc38181c47d2ae0852935569
ABohynDOE <alexandre.bohyn@kuleuven.be>
2020-11-06 16:05:03 +0100

    Added documentation

--- MATLAB/DOPselect.m
+++ MATLAB/DOPselect.m
@@ -3,21 +3,24 @@ function candicols = DOPselect(N,cols)
         N double {mustBeNumeric, mustBeInteger, mustBePositive}
         cols (1,:) {mustBeVector, mustBeNumeric, mustBeInRange(cols,1,N,"exclude-upper")}
     end
+    % DOPselect Generate candidate designs using the DOP-selection method.
+    %   DOPselect(N,cols) generates a matrix where each row contains the
+    %   columns for a candidate design, based on the columns in cols.
     outcols = zeros(N-1-log2(N),length(cols)+1);
     index = 1;
-    for ii = 1:N-1
-        if ismember(ii,cols)
+    for ii = 1:N-1 % All possible added factors
+        if ismember(ii,cols) % Reject if already used in the design
             continue
         end
-        wlp = zeros(length(cols)+1,N-1);
+        wlp = zeros(length(cols)+1,N-1); % Initiate WLP matrix
         for jj = 1:length(cols)+1
             t = [cols ii];
             t(jj) = [];
             w = WLP(t,2);
-            wlp(jj,:) = [w zeros(1,(N-1-length(w)))];
+            wlp(jj,:) = [w zeros(1,(N-1-length(w)))]; % Compute WLP for each DOP
         end
-        [~,idx] = sortrows(flip(wlp,1));
-        if idx(1) == 1
+        [~,idx] = sortrows(flip(wlp,1)); % Find the DOP with smallest aberration
+        if idx(1) == 1 % Keep candidate if parent designs has MA
             outcols(index,:) = [cols ii];
             index = index +1;
         end
--- MATLAB/STselect.m
+++ MATLAB/STselect.m
@@ -6,21 +6,33 @@ function candicols = STselect(N,cols,lastfac,order,sorted)
         order {mustBeText, mustBeMember(order,{'rL','cL'})} = 'rL'
         sorted {mustBeNumericOrLogical} = true
     end
+    % STselect Generate candidate designs using the search-table selection 
+    % method.
+    %   ST(N,cols,l) generates a matrix where each row contains the
+    %   columns for a candidate design, based on the columns in cols. 'l'
+    %   indicates the last column added in the parent designs.
+    %   ST(N,cols,l,order) generates the matrix of candidate designs, using
+    %   the order specified, which can be either conditional lexicographic
+    %   'cL' or reversed lexicogrpahic 'rL'. Default is 'rL'.
+    %   ST(N,cols,l,order,sorted) sorts the columns of the candidate
+    %   designs if sorted is true, and puts the last added columns in last 
+    %   position otherwise. Default is true.
     candicols = [];
     idx = 1;
+    % Initiate the different orders
     if strcmp(order,'cL')
         [~,ref] = sort(sum(de2bi(1:N-1),2));
     elseif strcmp(order,'rL')
         ref = 1:N-1;
     end
-    for ii = 1:N-1
-        if ismember(ii,cols)
+    for ii = 1:N-1 % All possible added columns
+        if ismember(ii,cols) % Reject if already used in the design
             continue
-        elseif find(ref==ii) <= find(ref==lastfac)
+        elseif find(ref==ii) <= find(ref==lastfac) % Reject if upper in the search-table
             continue
         end
         if sorted
-            outcols = sort([cols ii]);
+            outcols = sort([cols ii]); % Sort columns if needed
         else
             outcols = [cols ii];
         end
