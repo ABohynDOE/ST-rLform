22c38b7eca2d371098b52848e1355da7cfd78274
ABohynDOE <alexandre.bohyn@kuleuven.be>
2020-11-06 15:34:15 +0100

    Update doc

--- MATLAB/Catalog_Test.m
+++ MATLAB/Catalog_Test.m
@@ -1,7 +1,8 @@
 %% ST - rL-form combination
 % Input parameters
 N = 16; % Nbr of runs
-verbose = true;
+verbose = true; % Print info about enumeration
+maxfac = 15; % Max number of factors in the catalog
 
 % Initiate the catalog
 bf = 2.^(0:log2(N)-1); % basic factors
@@ -14,13 +15,13 @@ colsCell = {cols};
 
 % Iterate through values of k
 startcols = cols;
-for n = log2(N)+1:15
+for n = log2(N)+1:maxfac
     if verbose
         fprintf('%i factors\n',n)
     end
     k = n-log2(N);
     candicols = [];
-    % Candidate columns for first case
+    % Candidate columns for first added factor
     if n == log2(N)+1
         for ii = 1:N-1
             if ismember(ii,startcols)
@@ -28,10 +29,11 @@ for n = log2(N)+1:15
             end
             candicols = [candicols; [startcols ii]];
         end
-        % Candidate columns for second case
+    % Candidate columns for the rest of the added factors
     else
+        % Candidate columns selection
         for ii = 1:size(startcols,1)
-            tempcandicols = STselect(N,startcols(ii,:),startcols(ii,end),"rL",false);
+            tempcandicols = STselect(N,startcols(ii,:),startcols(ii,end),"rL",false); %Search-table for candidate columns
             candicols = [candicols; tempcandicols];
         end
     end
@@ -39,12 +41,13 @@ for n = log2(N)+1:15
         fprintf('\t%i candidates\n',size(candicols,1))
     end
     outcols = [];
+    % Isomorphism reduction of the candidate designs set
     for ii = 1:size(candicols,1)
-        if rLform(candicols(ii,:))
+        if rLform(candicols(ii,:)) % Only keep rL-minimal designs
             outcols = [outcols; candicols(ii,:)];
         end
     end
-    for ii = 1:size(outcols,1)
+    for ii = 1:size(outcols,1) % Add designs to the catalog
         nMat = [nMat; n];
         kMat = [kMat; k];
         colsCell{end+1} = outcols(ii,:);
@@ -55,3 +58,16 @@ for n = log2(N)+1:15
     end
 end
 T = table(nMat(:),kMat(:),colsCell','VariableNames',{'n','k','cols'});
+
+
+%% Summary table of the number of designs
+sumMat = zeros((maxfac-log2(N)),2);
+for ii = log2(N):maxfac
+    rows = T.n == ii;
+    sumMat(ii-log2(N)+1,1) = ii;
+    sumMat(ii-log2(N)+1,2) = numel(T(rows,1));
+end
+% Dipslay the table
+if verbose
+    sumMat
+end
