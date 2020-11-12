94e41d1b38b9bdbf0380401edf18ee71e7878093
ABohynDOE <alexandre.bohyn@kuleuven.be>
2020-11-12 14:13:17 +0100

    Doc update

--- MATLAB/Gmat.m
+++ MATLAB/Gmat.m
@@ -2,7 +2,7 @@ function G = Gmat(r)
     arguments
         r {mustBeNumeric, mustBePositive, mustBeInteger}
     end
-    % Gmat Generate the generalized reduced design matrix
+    % Gmat Generate the generalized reduced design matrix.
     %   G = Gmat(r) generates the r-by-(2^r)-1 generalized reduced design
     %       matrix for r basic factors.
     G = de2bi(1:(2^r)-1,'right-msb')';
