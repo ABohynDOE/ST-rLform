7238d94c09f412a9352993e985cbd10a82216e36
ABohynDOE <alexandre.bohyn@kuleuven.be>
2020-11-23 17:21:11 +0100

    No LF optimization

--- MATLAB/rLmin.m
+++ MATLAB/rLmin.m
@@ -12,12 +12,11 @@ function rL = rLmin(cols,lastfac)
     S = num2col(sort(cols)); % Reduced design matrix in rL order
     rL = true;
     rlst = nchoosek(1:size(S,2),size(S,1)); % All possible set of basic factors
+%     LFrows = any(rlst == find(cols == lastfac),2);
+%     rlst = rlst(LFrows,:);
     p = perms(1:size(S,1)); % All possible row permutations
     Lref = S(:,sum(S,1) > 1); % Added factors of the candidate design
     for ii = 1:length(rlst) % For each set of r basic factors
-%         if ~ismember(lastfac,rlst(ii,:)) % Reject if last factor not included
-%             continue
-%         end
         R = S(:,rlst(ii,:)); % New basic factor matrix
         if cond(R) >= 1/eps % Test conditional number for inversion
             continue; % Reject if it is singular
@@ -32,6 +31,9 @@ function rL = rLmin(cols,lastfac)
             [~,idx] = sort(col2num(Lstar));
             Lstarmin = Lstar(:,idx); % Sort the colums of L in rL-order
             if rLsmaller(Lstarmin,Lref) % Test if new L is rL-smaller
+                cols(rlst(ii,:))
+                R
+                Lstarmin
                 rL = false;
                 return
             end
