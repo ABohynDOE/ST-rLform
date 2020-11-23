7ae6cbe9856769ea7d9be07360924fc37c81b96a
ABohynDOE <alexandre.bohyn@kuleuven.be>
2020-11-23 17:21:32 +0100

    Update DOPselect.m
    Test with Eric's WLP characterization

--- MATLAB/DOPselect.m
+++ MATLAB/DOPselect.m
@@ -1,29 +1,24 @@
 function candicols = DOPselect(N,cols)
-    arguments
-        N double {mustBeNumeric, mustBeInteger, mustBePositive}
-        cols (1,:) {mustBeVector, mustBeNumeric, mustBeInRange(cols,1,N,"exclude-upper")}
-    end
+%     arguments
+%         N double {mustBeNumeric, mustBeInteger, mustBePositive}
+%         cols (1,:) {mustBeVector, mustBeNumeric, mustBeInRange(cols,1,N,"exclude-upper")}
+%     end
     % DOPselect Generate candidate designs using the DOP-selection method.
     %   DOPselect(N,cols) generates a matrix where each row contains the
     %   columns for a candidate design, based on the columns in cols.
     outcols = zeros(N-1-log2(N),length(cols)+1);
     index = 1;
-    for ii = 1:N-1 % All possible added factors
-        if ismember(ii,cols) % Reject if already used in the design
+    n = length(cols);
+    for ii = 1:N-1 % All possible added factors       if ismember(ii,cols) % Reject if already used in the design
             continue
         end
-        wlp = zeros(length(cols)+1,N-1); % Initiate WLP matrix
-        for jj = 1:length(cols)+1
-            t = [cols ii];
-            t(jj) = [];
-            w = WLP(t,2);
-            wlp(jj,:) = [w zeros(1,(N-1-length(w)))]; % Compute WLP for each DOP
-        end
-        [~,idx] = sortrows(flip(wlp,1)); % Find the DOP with smallest aberration
+        dop = nchoosek([cols ii],n);
+        wlp = WLPf(dop,3);
+        [~,idx] = sortrows(wlp,1); % Find the DOP with smallest aberration
         if idx(1) == 1 % Keep candidate if parent designs has MA
             outcols(index,:) = [cols ii];
             index = index +1;
         end
     end
-    candicols = outcols(sum(outcols,2)>0,:);
+    candicols = outcols(any(outcols,2),:);
 end
