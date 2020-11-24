6c4cf4a63e27cff9c335c91739b03bca9c2d3a6c
ABohynDOE <alexandre.bohyn@kuleuven.be>
2020-11-24 16:16:41 +0100

    easier col2num call
    matrix multiplication instead of for loop

--- MATLAB/col2num.m
+++ MATLAB/col2num.m
@@ -1,9 +1,5 @@
 function colvec = col2num(L)
     r = size(L,1);
-    n = size(L,2);
     rvec = 2.^(0:r-1);
-    colvec = zeros(1,n);
-    for ii = 1:n
-        colvec(ii) = rvec*L(:,ii);
-    end
+    colvec = rvec*L;
 end
