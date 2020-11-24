6eba8db573142174a686281e292a03bbe84fd4e4
ABohynDOE <alexandre.bohyn@kuleuven.be>
2020-11-24 16:17:14 +0100

    No setdiff
    faster with indexing

--- MATLAB/rLmin.m
+++ MATLAB/rLmin.m
@@ -1,7 +1,6 @@
-function rL = rLmin(cols,lastfac)
+function rL = rLmin(cols)
     arguments
         cols (1,:) {mustBeVector, mustBeNumeric, mustBePositive}
-        lastfac double {mustBeScalarOrEmpty, mustBeInteger, mustBePositive} = cols(end)
     end
     % rLmin Determine if a reduced design matrix in rL-form is rL-minimal.
     %   rLmin(cols) outputs true if the reduced design matrix, in rL-form, 
@@ -10,18 +9,20 @@ function rL = rLmin(cols,lastfac)
     %   column added to the candidate design.
     
     S = num2col(sort(cols)); % Reduced design matrix in rL order
+    n = size(S,2);
+    r = size(S,1);
     rL = true;
-    rlst = nchoosek(1:size(S,2),size(S,1)); % All possible set of basic factors
-%     LFrows = any(rlst == find(cols == lastfac),2);
-%     rlst = rlst(LFrows,:);
-    p = perms(1:size(S,1)); % All possible row permutations
+    rlst = nchoosek(1:n,r); % All possible set of basic factors
+    p = perms(1:r); % All possible row permutations
     Lref = S(:,sum(S,1) > 1); % Added factors of the candidate design
     for ii = 1:length(rlst) % For each set of r basic factors
         R = S(:,rlst(ii,:)); % New basic factor matrix
         if cond(R) >= 1/eps % Test conditional number for inversion
             continue; % Reject if it is singular
         end
-        K = S(:,setdiff(1:1:size(S,2),rlst(ii,:))); % Matrix of non-basic factors
+        Kcols = 1:n;
+        Kcols(rlst(ii,:)) = [];
+        K = S(:,Kcols); % Matrix of non-basic factors
         L = mod(R\K,2); % Recompute new added factors
         if any(any(~ismember(L,[0 1]))) % Checks that the L matrix is binary
             continue
@@ -31,9 +32,6 @@ function rL = rLmin(cols,lastfac)
             [~,idx] = sort(col2num(Lstar));
             Lstarmin = Lstar(:,idx); % Sort the colums of L in rL-order
             if rLsmaller(Lstarmin,Lref) % Test if new L is rL-smaller
-                cols(rlst(ii,:))
-                R
-                Lstarmin
                 rL = false;
                 return
             end
